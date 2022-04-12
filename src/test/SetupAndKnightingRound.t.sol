26825350496e7214bf9b62cf5ff555a2cd80543c
Fatimanaz <7fatimanaz@gmail.com>
2022-04-13 01:42:20 +0530

    feat: added tests for buy function of knightingRound.sol

--- src/test/SetupAndKnightingRound.t.sol
+++ src/test/SetupAndKnightingRound.t.sol
@@ -25,29 +25,54 @@ contract KnightingRoundTest is BaseFixture {
 
         // Users deposit assets
         vm.startPrank(shrimp);
+
+        vm.expectRevert("_tokenInAmount should be > 0");
+        knightingRound.buy(0, 0, emptyProof);
+
         comparator.snapPrev();
 
+        uint256 tokenOutAmountExpected = knightingRound.getAmountOut(1e8);
         wbtc.approve(address(knightingRound), wbtc.balanceOf(shrimp));
-        knightingRound.buy(1e8, 0, emptyProof);
+        uint256 tokenOutAmount = knightingRound.buy(1e8, 0, emptyProof);
         comparator.snapCurr();
+
+        assertEq(knightingRound.totalTokenIn(),1e8); // totalTokenIn should be equal to deposit
+        assertEq(tokenOutAmount, tokenOutAmountExpected); // transferred amount should be equal to expected
+        assertEq(knightingRound.totalTokenOutBought(), tokenOutAmount); 
+        assertEq(knightingRound.daoVotedFor(shrimp), 0); // daoVotedFor should be set
 
         assertEq(comparator.negDiff("wbtc.balanceOf(shrimp)"), 1e8);
         assertEq(comparator.diff("knightingRound.boughtAmounts(shrimp)"), 21e18);
 
+        // tokenInLimit = 100e8 so transaction should revert
+        vm.expectRevert("total amount exceeded");
+        knightingRound.buy(100e8, 0, emptyProof);
+        assertEq(knightingRound.totalTokenIn(),1e8); // totelTokenIn should be same 
+
+        // buying again
+
+        uint256 tokenOutAmount2 =knightingRound.buy(1e8, 0, emptyProof);
+        assertEq(knightingRound.totalTokenIn(),2e8); // should increment 
+        assertEq(knightingRound.totalTokenOutBought(), tokenOutAmount + tokenOutAmount2); 
+
+        // giving a different doa ID 
+        vm.expectRevert("can't vote for multiple daos");
+        knightingRound.buy(10e8, 1, emptyProof);
+        assertEq(knightingRound.totalTokenIn(),2e8); // totelTokenIn should be same
+        assertEq(knightingRound.daoVotedFor(shrimp), 0); // daoVotedFor should be same
+
         vm.stopPrank();
         
         // Knighting round concludes...
-        uint timeTillEnd = knightingRoundParams.start + knightingRoundParams.duration - block.timestamp;
-        vm.warp(timeTillEnd);
+        vm.warp(knightingRoundParams.start + knightingRoundParams.duration);
 
         // Can't buy after round ends
-        // vm.startPrank(shark);
-        // vm.expectRevert("KnightingRound: already ended");
-        // knightingRound.buy(1e8, 0, emptyProof);
-        // vm.stopPrank();
-
-        
+        vm.startPrank(shark);
+        vm.expectRevert("KnightingRound: already ended");
+        knightingRound.buy(1e8, 0, emptyProof);
+        vm.stopPrank();
     }
+    
 
     
 }
