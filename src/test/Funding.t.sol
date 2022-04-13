88d79315ed2696db2bc4958ed05dc4ff15dac1f7
sajanrajdev <sajanrajdev10@gmail.com>
2022-04-13 17:09:07 -0400

    test: linting and initial improvements

--- src/test/Funding.t.sol
+++ src/test/Funding.t.sol
@@ -15,9 +15,9 @@ contract FundingTest is BaseFixture {
     function setUp() public override {
         BaseFixture.setUp();
     }
-    
+
     function testDiscountRateBasics() public {
-        /** 
+    /*
         @fatima: confirm the discount rate is functional
         - access control for setting discount rate (i.e. the proper accounts can call the function and it works. improper accounts revert when attempting to call)
         - access control for setting discount rate limits
@@ -57,7 +57,7 @@ contract FundingTest is BaseFixture {
         (,minDiscount,maxDiscount,,,) = fundingCvx.funding();
 
         // checking if limits are set
-        assertEq(minDiscount,0);
+        assertEq(minDiscount, 0);
         assertEq(maxDiscount, 50);
 
         // check discount can not be greater than or equal to MAX_BPS
@@ -68,7 +68,7 @@ contract FundingTest is BaseFixture {
         // calling with wrong address
         vm.prank(address(1));
         vm.expectRevert(bytes("GAC: invalid-caller-role"));
-        fundingCvx.setDiscountLimits(0,20);
+        fundingCvx.setDiscountLimits(0, 20);
 
         // - pausing freezes these functions appropriately
         vm.prank(guardian);
@@ -82,19 +82,18 @@ contract FundingTest is BaseFixture {
 
     }
 
-    function testDiscountRateBuys() public{
-        _testDiscountRateBuys(fundingCvx, cvx, 100e18, 5000, 100e18 );
+    function testDiscountRateBuys() public {
+        _testDiscountRateBuys(fundingCvx, cvx, 100e18, 5000, 100e18);
 
     }
 
     function testBuyDifferentDecimals() public {
         // wBTC is an 8 decimal example
         // TODO: Fix comparator calls in inner function as per that functions comment
-        _testDiscountRateBuys(fundingWbtc, wbtc, 2e8, 2000, 2e8 );
-
+        _testDiscountRateBuys(fundingWbtc, wbtc, 2e8, 2000, 2e8);
     }
 
-    function testSetAssetCap() public{
+    function testSetAssetCap() public {
         vm.prank(address(1));
         vm.expectRevert("GAC: invalid-caller-role");
         fundingCvx.setAssetCap(10e18);
@@ -102,17 +101,38 @@ contract FundingTest is BaseFixture {
         // setting asset cap from correct account
         vm.prank(policyOps);
         fundingCvx.setAssetCap(1000e18);
-        (,,,,,uint256 assetCap) = fundingCvx.funding();
+        (,,,,, uint256 assetCap) = fundingCvx.funding();
         assertEq(assetCap, 1000e18); // check if assetCap is set
 
         // checking assetCap can not be less than accumulated funds.
-         _testDiscountRateBuys(fundingCvx, cvx, 100e18, 3000, 100e18 );
+         _testDiscountRateBuys(fundingCvx, cvx, 100e18, 3000, 100e18);
         vm.prank(policyOps);
         vm.expectRevert("cannot decrease cap below global sum of assets in");
         fundingCvx.setAssetCap(10e18);
+
+        // Attempt to deposit an amount over the cap reverts
+        vm.prank(whale);
+        cvx.approve(address(fundingCvx), 1000e18);
+        vm.expectRevert("asset funding cap exceeded");
+        fundingCvx.deposit(1000e18, 0);
+
+        // Increasing cap allows for deposit
+        vm.prank(policyOps);
+        fundingCvx.setAssetCap(10000e18);
+        (,,,,, assetCap) = fundingCvx.funding();
+        assertEq(assetCap, 10000e18); // check if assetCap is set
+
+        uint256 citadelAmountOutExpected = fundingCvx.getAmountOut(1000e18);
+        vm.prank(governance);
+        citadel.mint(address(fundingCvx), citadelAmountOutExpected + 1000); // fundingContract should have citadel to transfer to user
+
+        vm.startPrank(whale);
+        cvx.approve(address(fundingCvx), 1000e18);
+        uint256 citadelAmount = fundingCvx.deposit(1000e18, 0);
+        assertEq(citadelAmount, citadelAmountOutExpected);
     }
 
-    function testFailClaimAssetToTreasury() public{
+    function testFailClaimAssetToTreasury() public {
 
         vm.prank(address(1));
         vm.expectRevert("GAC: invalid-caller-role");
@@ -128,11 +148,10 @@ contract FundingTest is BaseFixture {
 
         // check the difference of saleRecipient's balance is equal to the amount
         assertEq(amount, balanceAfter-balanceBefore);
-
     }
 
     function testSweep() public {
-        
+
         vm.stopPrank();
         vm.prank(address(1));
         vm.expectRevert("GAC: invalid-caller-role");
@@ -141,11 +160,10 @@ contract FundingTest is BaseFixture {
         vm.prank(treasuryOps);
         vm.expectRevert("nothing to sweep");
         fundingCvx.sweep(address(cvx));
-
     }
-    function testAccessControl() public{
+
+    function testAccessControl() public {
         // tests to check access controls of various set functions
-        
         vm.prank(address(1));
         vm.expectRevert("GAC: invalid-caller-role");
         fundingCvx.setDiscountManager(address(2));
@@ -164,7 +182,7 @@ contract FundingTest is BaseFixture {
         vm.prank(eoaOracle);
         fundingCvx.updateCitadelPriceInAsset(1000);
         assertEq(fundingCvx.citadelPriceInAsset(), 1000); // check if citadelPriceInAsset is set
-        
+
         vm.prank(eoaOracle);
         vm.expectRevert("citadel price must not be zero");
         fundingCvx.updateCitadelPriceInAsset(0);
@@ -177,18 +195,18 @@ contract FundingTest is BaseFixture {
         vm.prank(governance);
         fundingCvx.setSaleRecipient(address(2));
         assertEq(fundingCvx.saleRecipient(), address(2)); // check if SaleRecipient is set
-        
+
         vm.prank(governance);
         vm.expectRevert("Funding: sale recipient should not be zero");
         fundingCvx.setSaleRecipient(address(0));
     }
-    
-    function testDepositModifiers() public{
+
+    function testDepositModifiers() public {
         // pausing should freeze deposit
         vm.prank(guardian);
         gac.pause();
         vm.expectRevert(bytes("global-paused"));
-        fundingCvx.deposit(10e18 , 0);
+        fundingCvx.deposit(10e18, 0);
         vm.prank(address(techOps));
         gac.unpause();
 
@@ -198,22 +216,21 @@ contract FundingTest is BaseFixture {
         vm.prank(eoaOracle);
         fundingCvx.updateCitadelPriceInAsset(6000);
         vm.expectRevert(bytes("Funding: citadel price from oracle flagged and pending review"));
-        fundingCvx.deposit(10e18 , 0);
-
+        fundingCvx.deposit(10e18, 0);
     }
 
     function _testDiscountRateBuys(Funding fundingContract, IERC20 token, uint256 _assetAmountIn, uint32 discount, uint256 citadelPrice) public {
-        
-        /**
+
+        /*
             @fatima: this is a good candidate to generalize using fuzzing: test buys with various discount rates, using fuzzing, and confirm the results.
             sanity check the numerical results (tokens in vs tokens out, based on price and discount rate)
-        */ 
+        */
 
-        vm.assume(discount<10000 && _assetAmountIn>0 && citadelPrice>0);  // discount < MAX_BPS = 10000 
+        vm.assume(discount<10000 && _assetAmountIn>0 && citadelPrice>0);  // discount < MAX_BPS = 10000
 
         vm.prank(address(governance));
         fundingContract.setDiscountLimits(0, 9999);
-        
+
         vm.prank(address(policyOps));
         fundingContract.setDiscount(discount); // set discount
 
@@ -223,31 +240,33 @@ contract FundingTest is BaseFixture {
         uint256 citadelAmountOutExpected = fundingContract.getAmountOut(_assetAmountIn);
 
         vm.prank(governance);
-        citadel.mint(address(fundingContract), citadelAmountOutExpected ); // fundingContract should have citadel to transfer to user
+        citadel.mint(address(fundingContract), citadelAmountOutExpected); // fundingContract should have citadel to transfer to user
 
-        address user = address(1) ;
+        address user = address(1);
         vm.startPrank(user);
-        erc20utils.forceMintTo(user, address(token) , _assetAmountIn );
+        erc20utils.forceMintTo(user, address(token), _assetAmountIn);
         token.approve(address(fundingContract), _assetAmountIn);
-        uint256 citadelAmountOut = fundingContract.deposit(_assetAmountIn , 0);
+        uint256 citadelAmountOut = fundingContract.deposit(_assetAmountIn, 0);
         vm.stopPrank();
 
         // check citadelAmoutOut is same as expected
-        assertEq(citadelAmountOut , citadelAmountOutExpected);
+        assertEq(citadelAmountOut, citadelAmountOutExpected);
 
     }
-    
+
     function _testBuy(Funding fundingContract, uint assetIn, uint citadelPrice) internal {
         // just make citadel appear rather than going through minting flow here
         erc20utils.forceMintTo(address(fundingContract), address(citadel), 100000e18);
-        
+
         vm.prank(eoaOracle);
 
-        // CVX funding contract gives us an 18 decimal example
+        // CVX funding contract gives us a 18 decimal example
         fundingContract.updateCitadelPriceInAsset(citadelPrice);
 
         uint expectedAssetOut = assetIn.divWadUp(citadelPrice);
-        
+
+        uint256 citadelAmountOutExpected = fundingContract.getAmountOut(assetIn);
+
         emit log_named_uint("Citadel Price", citadelPrice);
 
         vm.startPrank(whale);
@@ -258,24 +277,22 @@ contract FundingTest is BaseFixture {
         comparator.snapPrev();
         cvx.approve(address(fundingContract), cvx.balanceOf(whale));
 
-        fundingContract.deposit(assetIn, 0);
+        uint256 citadelAmountOut = fundingContract.deposit(assetIn, 0);
         comparator.snapCurr();
-
-        uint expectedAssetLost = assetIn;
-        uint expectedxCitadelGained = citadelPrice;
 
         // user trades in asset for citadel in xCitadel form.
         assertEq(comparator.diff("citadel.balanceOf(whale)"), 0);
         assertEq(comparator.diff("xCitadel.balanceOf(whale)"), expectedAssetOut);
         assertEq(comparator.negDiff("cvx.balanceOf(whale)"), assetIn);
-        
-        // funding contract loses citadel and sends asset to saleRecipient. should never hold an xCitadel balance (deposited for each user) (gas costs?)
+
+        // funding contract loses citadel and sends asset to saleRecipient. should never hold a xCitadel balance (deposited for each user) (gas costs?)
 
         // TODO: Improve comparator to easily add new entity for all balance calls.
         assertEq(comparator.negDiff("citadel.balanceOf(fundingCvx)"), expectedAssetOut);
         assertEq(comparator.diff("cvx.balanceOf(treasuryVault)"), assetIn);
-        
+
         assertEq(xCitadel.balanceOf(address(fundingContract)), 0);
+        assertEq(citadelAmountOut, citadelAmountOutExpected);
 
         vm.stopPrank();
     }
