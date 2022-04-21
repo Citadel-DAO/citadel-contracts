11d31386c2122dae708dca9ec037dafd4de9d072
sajanrajdev <sajanrajdev10@gmail.com>
2022-04-21 13:49:00 -0400

    fix: library paths to remaps

--- src/mocks/CVX.sol
+++ src/mocks/CVX.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
 contract Convex is ERC20 {
     constructor() ERC20("Convex", "CVX") {}
