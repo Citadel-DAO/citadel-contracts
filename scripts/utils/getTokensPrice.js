16d3fb225351010e32fe3a07fbf6e9d80cafa968
Ajand Mardalizad <aseedgeek@gmail.com>
2022-04-28 21:04:08 +0430

    get tokens price from coingecko added

--- scripts/utils/getTokensPrice.js
+++ scripts/utils/getTokensPrice.js
@@ -1,4 +1,5 @@
 const { ethers } = require("ethers");
+const axios = require("axios");
 
 const calcTokenoutPrice = (desiredPriceInUsd, priceInUsd, decimals) => {
   return ethers.BigNumber.from(desiredPriceInUsd)
@@ -6,4 +7,27 @@ const calcTokenoutPrice = (desiredPriceInUsd, priceInUsd, decimals) => {
     .div(ethers.BigNumber.from(priceInUsd));
 };
 
+const getTokensPrices = (tokensList) => {
+  const reqUrl = `
+https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokensList.join(
+    ","
+  )}&vs_currencies=usd
+`;
 
+  console.log(reqUrl);
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(reqUrl)
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
+};
+
+module.exports = { calcTokenoutPrice, getTokensPrices };
