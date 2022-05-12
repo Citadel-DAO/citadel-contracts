47940d4dedd084b86763d7784d9f849b100cc561
sajanrajdev <sajanrajdev10@gmail.com>
2022-05-12 17:14:03 -0400

    feat: added asset generation

--- src/test/AtomicLaunchTest.t.sol
+++ src/test/AtomicLaunchTest.t.sol
@@ -6,10 +6,26 @@ import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 import "../interfaces/erc20/IERC20.sol";
 
+// General interfaces for special non-ERC20 tokens' mint function
+interface ISpecialMinter {
+    function mint(address account, uint256 amount) external;
+}
+
+interface IUSDCMasterMinter {
+    function incrementMinterAllowance(uint256 _allowanceIncrement) external;
+}
+
 contract AtomicLaunchTest is BaseFixture {
     using FixedPointMathLib for uint256;
 
     uint256 constant MAX_UINT256 = type(uint256).max;
+
+    address constant renBTC_owner = 0xe4b679400F0f267212D5D812B95f58C83243EE71;
+    address constant ust_owner = 0x3ee18B2214AFF97000D974cf647E7C347E8fa585;
+    address constant usdcMasterMinter = 0xE982615d461DD5cD06575BbeA87624fda4e3de17;
+    address constant usdcMasterMinter_owner = 0x79E0946e1C186E745f1352d7C21AB04700C99F71;
+    address constant usdc_owner = 0x5B6122C109B78C6755486966148C1D70a50A47D7;
+    address constant badger_treasury = 0xD0A7A8B98957b9CD3cFB9c0425AbE44551158e9e;
 
     // NOTE: wBTC and wETH are handled on fixture
     KnightingRound knightingRound_cvx = new KnightingRound();
@@ -132,13 +148,86 @@ contract AtomicLaunchTest is BaseFixture {
         );
 
         vm.stopPrank();
+
+        // Mint assets for users
+        address btc_user = address(1);
+        address stable_user = address(2);
+        address influence_user = address(3);
+        address eth_user = address(4);
+
+        // Mint BTC based assets
+        erc20utils.forceMintTo(btc_user, wbtc_address, 1000e8);
+        erc20utils.forceMintTo(btc_user, ibBTC_address, 1000e18);
+        vm.prank(renBTC_owner); // RenBTC Owner
+        ISpecialMinter(renBTC_address).mint(btc_user, 1000e8);
+
+        // Mint Stablecoin assets
+        erc20utils.forceMintTo(stable_user, frax_address, 100000e18);
+        vm.prank(ust_owner); // UST Owner
+        ISpecialMinter(ust_address).mint(stable_user, 100000e6);
+        vm.prank(usdcMasterMinter_owner);
+        IUSDCMasterMinter(usdcMasterMinter).incrementMinterAllowance(
+            100000e6
+        );
+        vm.prank(usdc_owner); // USDC Owner
+        ISpecialMinter(usdc_address).mint(stable_user, 100000e6);
+
+        // // Mint influence assets
+        erc20utils.forceMintTo(influence_user, cvx_address, 10000e18);
+        vm.startPrank(badger_treasury);
+        bveCVX.transfer(influence_user, bveCVX.balanceOf(badger_treasury));
+        badger.transfer(influence_user, badger.balanceOf(badger_treasury));
+        require(bveCVX.balanceOf(influence_user) > 0, "No bveCVX tranferred");
+        require(badger.balanceOf(influence_user) > 0, "No BADGER tranferred");
+        vm.stopPrank();
+
+        // Deal ETH
+        vm.deal(eth_user, 100 ether);
     }
 
     function testAtomicLaunch() public {
         require(true, "test setup");
     }
 
-    // function emulateKnightingRound() {
+    function _simulateeKnightingRound() public {
+        bytes32[] memory emptyProof = new bytes32[](1);
 
-    // }
+        // Move to knighting round start
+        vm.warp(knightingRound.saleStart());
+
+        // Shrimp BTC
+        vm.startPrank(shrimp);
+        wbtc.approve(address(knightingRound), wbtc.balanceOf(shrimp));
+        weth.approve(address(knightingRound), address(shrimp).balance);
+
+        knightingRound.buy(wbtc.balanceOf(shrimp) / 2, 0, emptyProof);
+
+        //Shrimp ETH
+        knightingRoundWithEth.buyEth{value: address(shrimp).balance / 2}(
+            0,
+            emptyProof
+        );
+
+        vm.stopPrank();
+
+        // Whale BTC
+        vm.startPrank(whale);
+        wbtc.approve(address(knightingRound), wbtc.balanceOf(whale));
+        weth.approve(address(knightingRound), address(whale).balance);
+
+        knightingRound.buy(wbtc.balanceOf(whale) / 2, 0, emptyProof);
+
+        //Whale ETH
+        knightingRoundWithEth.buyEth{value: address(whale).balance / 2}(
+            0,
+            emptyProof
+        );
+
+        vm.stopPrank();
+        // Knighting round concludes...
+        uint256 timeTillEnd = knightingRoundParams.start +
+            knightingRoundParams.duration -
+            block.timestamp;
+        vm.warp(timeTillEnd);
+    }
 }
