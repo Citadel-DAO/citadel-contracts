69730820d25f89dc123dfd20df15e442d5d0bb24
Petrovska <petrovska@badger.finance>
2022-06-09 23:31:25 -0100

    Introduce the oracle deployment directly from contract

--- src/AtomicLaunch.sol
+++ src/AtomicLaunch.sol
@@ -5,11 +5,15 @@ pragma solidity 0.8.12;
 import "openzeppelin-contracts/utils/structs/EnumerableSet.sol";
 
 import {ChainlinkUtils} from "./oracles/ChainlinkUtils.sol";
+import {CtdlWbtcCurveV2Provider} from "./oracles/CtdlWbtcCurveV2Provider.sol";
+import {CtdlAssetChainlinkProvider} from "./oracles/CtdlAssetChainlinkProvider.sol";
+import {CtdlBtcChainlinkProvider} from "./oracles/CtdlBtcChainlinkProvider.sol";
+import {CtdlEthChainlinkProvider} from "./oracles/CtdlEthChainlinkProvider.sol";
+import {CtdlWibbtcLpVaultProvider} from "./oracles/CtdlWibbtcLpVaultProvider.sol";
 
 import "./interfaces/erc20/IERC20.sol";
 import "./interfaces/curve/ICurvePoolFactory.sol";
 import "./interfaces/curve/ICurvePool.sol";
-import "./interfaces/citadel/IMedianOracleProvider.sol";
 import "./interfaces/chainlink/IAggregatorV3Interface.sol";
 
 contract AtomicLaunch is ChainlinkUtils {
@@ -17,11 +21,43 @@ contract AtomicLaunch is ChainlinkUtils {
 
     address public citadel;
     address public wbtc;
+    address public gac;
+    address public xCTDL;
+    address public treasury;
+
+    address public constant WBTC_BTC_PRICE_FEED =
+        0xfdFD9C85aD200c506Cf9e21F1FD8dd01932FBB23;
+    address public constant BTC_ETH_PRICE_FEED =
+        0xdeb288F737066589598e9214E782fa5A8eD689e8;
+
+    address public constant WIBBTC_LP_VAULT =
+        0xaE96fF08771a109dc6650a1BdCa62F2d558E40af;
+
+    address public constant FRAX_ETH_PRICE_FEED =
+        0x14d04Fff8D21bd62987a5cE9ce543d2F1edF5D3E;
+    address public constant FRAX_USD_PRICE_FEED =
+        0xB9E1E3A9feFf48998E45Fa90847ed4D467E8BcfD;
+
+    address public constant USDC_ETH_PRICE_FEED =
+        0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
+    address public constant USDC_USD_PRICE_FEED =
+        0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+
+    address public constant CVX_ETH_PRICE_FEED =
+        0xC9CbF687f43176B302F03f5e58470b77D07c61c6;
+    address public constant CVX_USD_PRICE_FEED =
+        0xd962fC30A72A84cE50161031391756Bf2876Af5D;
+
+    address public constant BADGER_ETH_PRICE_FEED =
+        0x58921Ac140522867bf50b9E009599Da0CA4A2379;
+    address public constant BADGER_USD_PRICE_FEED =
+        0x66a47b7206130e6FF64854EF0E1EDfa237E65339;
+
     address public constant CURVE_POOL_FACTORY =
         0xF18056Bbd320E96A48e3Fbf8bC061322531aac99;
     uint256 public constant CITADEL_PRICE = 21;
 
-    IAggregatorV3Interface public wbtcBtcPriceFeed =
+    IAggregatorV3Interface public wbtcUsdPriceFeed =
         IAggregatorV3Interface(0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c);
 
     /* ========== STATE VARIABLES ========== */
@@ -36,11 +72,17 @@ contract AtomicLaunch is ChainlinkUtils {
     constructor(
         address _governance,
         address _citadel,
-        address _wbtc
+        address _wbtc,
+        address _gac,
+        address _xCTDL,
+        address _treasury
     ) {
         governance = _governance;
         citadel = _citadel;
         wbtc = _wbtc;
+        gac = _gac;
+        xCTDL = _xCTDL;
+        treasury = _treasury;
     }
 
     /***************************************
@@ -56,15 +98,9 @@ contract AtomicLaunch is ChainlinkUtils {
                ADMIN - GOVERNANCE
     ****************************************/
 
-    function addOracle(address _oracleAddress) external onlyGovernance {
-        require(_oracleAddress != address(0), "zero-address!");
-        _oracles.add(_oracleAddress);
-    }
-
     function launch(uint256 citadelToLiquidity, uint256 wbtcToLiquidity)
         external
         onlyGovernance
-        returns (address poolAddress)
     {
         uint256 initialPoolPrice = _initialPoolPrice();
 
@@ -117,7 +153,9 @@ contract AtomicLaunch is ChainlinkUtils {
             "<pool-curve-wbtcToLiquidity!"
         );
 
-        _setCurvePoolInOracles(poolAddress);
+        _deployOracles(poolAddress);
+
+        //_fundingInitialize();
 
         // NOTE: mainly for clean event, so no needs for mega-detective work
         emit PoolCreated(poolAddress);
@@ -125,21 +163,88 @@ contract AtomicLaunch is ChainlinkUtils {
 
     function _initialPoolPrice() internal returns (uint256 _poolPrice) {
         (uint256 wbtcOraclePricing, , bool valid) = safeLatestAnswer(
-            wbtcBtcPriceFeed
+            wbtcUsdPriceFeed
         );
 
         require(valid, "not-valid!");
 
-        uint256 wbtcPrice = wbtcOraclePricing / 10**wbtcBtcPriceFeed.decimals();
+        uint256 wbtcPrice = wbtcOraclePricing / 10**wbtcUsdPriceFeed.decimals();
 
         _poolPrice = (wbtcPrice / CITADEL_PRICE) * 1e18;
     }
 
-    function _setCurvePoolInOracles(address _ctdlWbtcCurvePool) internal {
-        for (uint256 i = 0; i < _oracles.length(); i++) {
-            IMedianOracleProvider(_oracles.at(i)).setCurvePool(
+    function _deployOracles(address _ctdlWbtcCurvePool) internal {
+        CtdlWbtcCurveV2Provider ctdlWbtcProviderLoc = new CtdlWbtcCurveV2Provider(
                 _ctdlWbtcCurvePool
             );
+        _oracles.add(address(ctdlWbtcProviderLoc));
+
+        CtdlBtcChainlinkProvider ctdlBtcProvider = new CtdlBtcChainlinkProvider(
+            _ctdlWbtcCurvePool,
+            WBTC_BTC_PRICE_FEED
+        );
+        _oracles.add(address(ctdlBtcProvider));
+
+        CtdlWibbtcLpVaultProvider ctdlWibbtcProvider = new CtdlWibbtcLpVaultProvider(
+                _ctdlWbtcCurvePool,
+                WBTC_BTC_PRICE_FEED,
+                WIBBTC_LP_VAULT
+            );
+        _oracles.add(address(ctdlWibbtcProvider));
+
+        CtdlEthChainlinkProvider ctdlEthProvider1 = new CtdlEthChainlinkProvider(
+                _ctdlWbtcCurvePool,
+                WBTC_BTC_PRICE_FEED,
+                BTC_ETH_PRICE_FEED
+            );
+        _oracles.add(address(ctdlEthProvider1));
+
+        CtdlAssetChainlinkProvider ctdlEthProvider2 = new CtdlAssetChainlinkProvider(
+                _ctdlWbtcCurvePool,
+                WBTC_BTC_PRICE_FEED,
+                address(wbtcUsdPriceFeed),
+                ETH_USD_PRICE_FEED
+            );
+        _oracles.add(address(ctdlEthProvider2));
+
+        address[4] memory assetEthFeeds = [
+            FRAX_ETH_PRICE_FEED,
+            USDC_ETH_PRICE_FEED,
+            CVX_ETH_PRICE_FEED,
+            BADGER_ETH_PRICE_FEED
+        ];
+
+        address[4] memory assetUsdFeeds = [
+            FRAX_USD_PRICE_FEED,
+            USDC_USD_PRICE_FEED,
+            CVX_USD_PRICE_FEED,
+            BADGER_USD_PRICE_FEED
+        ];
+
+        for (uint256 i; i < 4; ++i) {
+            CtdlAssetChainlinkProvider ctdlAssetProvider1 = new CtdlAssetChainlinkProvider(
+                    _ctdlWbtcCurvePool,
+                    WBTC_BTC_PRICE_FEED,
+                    BTC_ETH_PRICE_FEED,
+                    assetUsdFeeds[i]
+                );
+            _oracles.add(address(ctdlAssetProvider1));
+
+            CtdlAssetChainlinkProvider ctdlAssetProvider2 = new CtdlAssetChainlinkProvider(
+                    CTDL_WBTC_CURVE_POOL,
+                    WBTC_BTC_PRICE_FEED,
+                    address(wbtcUsdPriceFeed),
+                    assetEthFeeds[i]
+                );
+            _oracles.add(address(ctdlAssetProvider2));
         }
+    }
+
+    /***************************************
+               PUBLIC FUNCTION
+    ****************************************/
+
+    function getAllOracles() public view returns (address[] memory) {
+        return _oracles.values();
     }
 }
